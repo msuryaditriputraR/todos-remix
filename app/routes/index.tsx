dfd871abbd845cd35754c4b80b75da2ecd7a4552
M Haidar Hanif <git@mhaidarhanif.com>
2023-01-27 21:12:52 +0700

    feat: setup prisma lib, find todos from prisma

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,15 @@
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { prisma } from "~/libs/prisma";
 
 export async function loader() {
-  const todos = [
-    { id: 1, text: "Todo A" },
-    { id: 2, text: "Todo B" },
-    { id: 3, text: "Todo C" },
-  ];
+  const todos = await prisma.todo.findMany();
+
+  // const todos = [
+  //   { id: 1, text: "Todo A" },
+  //   { id: 2, text: "Todo B" },
+  //   { id: 3, text: "Todo C" },
+  // ];
   return json({ todos });
 }
 
